6ab31e57be3e3706a26a141a3598469a903fc290
exploratoryprorammer <rohan.k.sah@gmail.com>
2025-10-15 00:11:42 -0500

    fixed mongodb connection

--- db/mongo.go
+++ db/mongo.go
@@ -3,8 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
-	"log"
-	"time"
+
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -14,28 +13,24 @@ import (
 var Client *mongo.Client
 
 func ConnectDB() *mongo.Client {
-	// Replace <db_password> with your actual password
-	uri := "mongodb+srv://rksah:<db_password>@cluster0.kac1fxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-	// Use the SetServerAPIOptions() method to set the version of the Stable API
+	uri := "mongodb+srv://rksah:isfsupporters@cluster0.kac1fxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(ctx, opts)
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		log.Fatal("Failed to connect to MongoDB:", err)
+    panic(err)
 	}
-
+	defer func() {
+	if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+	}()
 	// Send a ping to confirm a successful connection
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		log.Fatal("Could not ping MongoDB:", err)
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	panic(err)
 	}
-
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-
 	Client = client
 	return Client
 }
